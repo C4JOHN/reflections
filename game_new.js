2212d278eeff47c69b3cf4e12e8cf8ae5d03ebcf
sirJohn <emoavwoduajohn@gmail.com>
2017-12-14 13:23:46 -0600

    some more comments

--- game_new.js
+++ game_new.js
@@ -17,6 +17,8 @@ KEY_CODES = {
 }
 // just checking what commits I have made
 
+// adding more comments to the file
+
 KEY_STATUS = { keyDown:false };
 for (code in KEY_CODES) {
   KEY_STATUS[KEY_CODES[code]] = false;
